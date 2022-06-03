d0bc81f7f48c8f3a1b4ad12eaab2008c6dfe3851
jonibi8787 <jonibi8787@.com>
2022-06-03 00:05:49 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/*  main function */
 int main(void)
 {
+/* the out put is "Programming is like building a multili */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
