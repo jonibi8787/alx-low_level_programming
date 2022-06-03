4ee2b72fa69a31f72389ce7cf0efca934bda8a82
jonibi8787 <jonibi8787@.com>
2022-06-03 03:40:34 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/*  main function */
-int main(void)
+
+int main()
 {
-/* the out put is "Programming is like building a multil
- * return 0
- * pk */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
