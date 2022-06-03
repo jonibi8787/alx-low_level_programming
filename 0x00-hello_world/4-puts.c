b9156ee62f19ca1fc0619a32fdb2d2e7ee6d3fda
jonibi8787 <jonibi8787@.com>
2022-06-03 03:37:47 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,9 @@
 /*  main function */
 int main(void)
 {
-/* the out put is "Programming is like building a multili */
+/* the out put is "Programming is like building a multil
+ * return 0
+ * pk */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
