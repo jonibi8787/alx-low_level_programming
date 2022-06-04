7f1632721a6d4f19245f5e7214b79f07cc89c8ee
jonibi8787 <jonibi8787@.com>
2022-06-04 09:51:11 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-int main()
+
+
+/* betty style doc for function main goes there */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
