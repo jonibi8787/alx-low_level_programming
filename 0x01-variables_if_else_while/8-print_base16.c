7b20474ac345a08f960586077eca983a55fbf442
jonibi8787 <jonibi8787@.com>
2022-06-05 12:29:57 -0700

    number and letter

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	for (m = 'a'; n <= 'f'; m++)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+
+	return (0);
+}
