c6d24a3067159e481000c19524bd6aeebae2b24b
jonibi8787 <jonibi8787@.com>
2022-06-05 12:23:23 -0700

    letter

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n <= 'a'; n--)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+
+	return (0);
+}
