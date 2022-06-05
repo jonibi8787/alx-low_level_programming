edaa2994d09c6ccb9da8d94242263c462ca4ec08
jonibi8787 <jonibi8787@.com>
2022-06-05 11:23:20 -0700

    continue

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n == 'e' && n == 'q')
+			continue;
+		putchar(n);
+	}
+		putchar('\n');
+
+	return (0);
+}
+© 2022 GitHub, Inc.
