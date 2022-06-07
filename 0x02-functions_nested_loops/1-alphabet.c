5e3ca4d1f99a8ecd1f6754e3f7a3eb4df9f13cfa
jonibi8787 <jonibi8787@.com>
2022-06-07 06:53:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+\**
+ * main - check the code
+ * Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+		_putchar('/n');
+	}
+}
