d38461e51691c2732f31de6cad6916608c4ec516
jonibi8787 <jonibi8787@.com>
2022-06-07 11:04:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <stdio.h>
-#include <time.h>
 #include "main.h"
-\**
+
+/**
  * main - check the code
+ *
  * Return: Always 0.
-*/
-void print_alphabet(void)
+ */
+int main(void)
 {
 	char n;
 
@@ -14,5 +13,6 @@ void print_alphabet(void)
 	{
 		_putchar(n);
 		_putchar('/n');
+		return (0);
 	}
 }
