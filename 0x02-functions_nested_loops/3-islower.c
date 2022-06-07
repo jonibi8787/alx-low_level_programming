d6db085f3e1f73657aacddeeec599852b536bd13
jonibi8787 <jonibi8787@.com>
2022-06-07 11:55:05 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Function checks is lowercase character
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if c is lowercase else return 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
