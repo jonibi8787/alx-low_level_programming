5a7f62b48d7651bb8130434e138fd1c2f267df0b
jonibi8787 <jonibi8787@.com>
2022-06-07 11:37:50 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Functions prints the alphabet in lowercase
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char l;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+			_putchar('\n');
+		}
+	}
+}
