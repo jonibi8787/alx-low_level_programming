7554fb23e1cf168508352c7fe5432a007de414f9
jonibi8787 <jonibi8787@.com>
2022-06-07 17:47:04 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h">
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @c: number to check
+ *
+ * Return: +1 if is positive, 0 if is 0 and -1 if is negative
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+		return (1);
+	printf("+");
+	else if (c == 0)
+		return (-1);
+	printf("0");
+	else
+		return (-1);
+	printf("-");
+}
