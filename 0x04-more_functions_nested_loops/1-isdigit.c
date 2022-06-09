c1cc7145d4a1572897b4bbd3eb9cbf29bd7011f4
jonibi8787 <jonibi8787@.com>
2022-06-09 11:47:21 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - check the code
+ * @c: check the code
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+	{
+	return(1);
+	}
+	else
+	{
+	return(0);
+	}
+}	
