332742e4e8d66410c90930f8773000618133996c
jonibi8787 <jonibi8787@.com>
2022-06-09 12:39:00 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; i++)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c);
+		}
+	}
+		_putchar('\n');
+}
