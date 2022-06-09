eee20bff06a18f8f4d8997433521efd0019abddd
jonibi8787 <jonibi8787@.com>
2022-06-09 13:12:12 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '14'; n++)
+	{
+		_putchar(n);
+		_putchar('\n';
+	}
+}
+
