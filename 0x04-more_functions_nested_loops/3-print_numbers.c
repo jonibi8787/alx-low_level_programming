1c26e34c4581cfaa586e721e789b24f1c2090a13
jonibi8787 <jonibi8787@.com>
2022-06-09 12:29:55 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
