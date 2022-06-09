6c6f8bee585b704d5692feaae9450f4fec21385b
jonibi8787 <jonibi8787@.com>
2022-06-09 13:14:42 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,17 @@
  */
 void more_numbers(void)
 {
-	char n;
+	int i;
+	char c;
 
-	for (n = '0'; n <= '14'; n++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(n);
-		_putchar('\n';
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
-
