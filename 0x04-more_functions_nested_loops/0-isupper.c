6813d2d393c95b58a9e4cacea208c2965bf4f934
jonibi8787 <jonibi8787@.com>
2022-06-09 05:02:03 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check the code.
+ * @c: number to check
+ * Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+	char c;
+
+	c = 'A';
+	if (c == _isupper(c)
+	return (1);
+
+	c = 'a';
+	if (c == _isupper(c)
+
+	return (0);
+
+}
