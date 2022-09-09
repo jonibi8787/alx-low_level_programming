234d8cc64b079eafbc1bf77dab2b298ad351e3c7
jonibi8787 <jonibi8787@.com>
2022-09-09 09:14:08 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/amarapeace/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+cp ../sh2.sh .
+. ./sh2.sh
